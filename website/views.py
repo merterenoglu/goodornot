89fb6c4e31276a6b90d8269de6d567814c278e0a
ogulcan <ogulcanalicanturk@gmail.com>
2015-12-20 22:23:49 +0200

    views tamm

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Images,Comments
+def listPictures(request):
+    return render(request, ‘pictures.html’, {image:’image’}) 
+def    loadImages():
+ imagesHTML = ""
+ for image in Image.objects.all():
+    imagesHTML += '<img src="'+image.idl+'">'
+ return imagesHTML
 
 # Create your views here.
