01e38554f1b8f0b446c7e1b41d3ab2a62ee9c2a2
ogulcanalicanturk <ogulcanalicanturk@gmail.com>
2015-12-22 15:23:47 +0200

    duzeltme

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ def listPictures(request):
     return render(request, ‘pictures.html’, {image:loadImages()}) 
 def    loadImages():
  return Images.objects.all()
->>>>>>> 6fee7dd02c0aad902ff9c254607605255c923290
+
 
 def loadComments():
  return Comments.objects.all()
